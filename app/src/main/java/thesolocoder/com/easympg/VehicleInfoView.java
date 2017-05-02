9d561a56fa54332ddf819eb192cdbeb7a0e0d3f3
mikebal <darekmichael@gmail.com>
2017-05-01 23:05:13 -0600

    Added required fields to vehicleinfo layout.  Added a button to the menu bar of vehicleinfo

--- app/src/main/java/thesolocoder/com/easympg/VehicleInfoView.java
+++ app/src/main/java/thesolocoder/com/easympg/VehicleInfoView.java
@@ -2,14 +2,36 @@ package thesolocoder.com.easympg;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
 public class VehicleInfoView extends AppCompatActivity{
+
+    MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vehicleinfo);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menuItem=menu.add("confirm");
+        menuItem.setIcon(R.mipmap.ic_check_white_36dp);
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getTitle().equals("confirm"))
+            confirmButtonClicked();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void confirmButtonClicked(){
+
     }
 }
